79b39e167d2c3ce6506e8a71155b5261a2a2facf
Brunofell <brunofelimarti@gmail.com>
2024-02-01 17:41:14 -0300

    sla

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,5 +1,10 @@
 package med.voll.api.controller;
 
+import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.Endereco;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,18 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
+    @Autowired
+    private MedicoRepository repository;
 
     @PostMapping
-    public void cadastrar(@RequestBody String msg){
-        System.out.println(msg);
+    public void cadastrar(@RequestBody DadosCadastroMedico dados){
+        repository.save(new Medico(dados));
     }
 
 
-//    Nome
-//    E-mail
-//    Telefone
-//    CRM
-//    Especialidade (Ortopedia, Cardiologia, Ginecologia ou Dermatologia)
-//    Endereço completo (logradouro, número, complemento, bairro, cidade, UF e CEP)
 
 }
---
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,34 @@
+package med.voll.api.medico;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "medicos")
+@Entity(name = "Medico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+
+    public Medico(DadosCadastroMedico dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.crm = dados.crm();
+        this.especialidade = dados.especialidade();
+        this.endereco = new Endereco(dados.endereco());
+    }
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded // fica numa class separada mas no bd faz parte da mesma tabela
+    private Endereco endereco;
+
+}
